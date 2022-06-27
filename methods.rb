344b463cc3d618b113a20368c0263f6cd5ecc8dd
Griffin Dooley <dooleygriffin@gmail.com>
2022-06-27 10:49:48 -0400

    commit tree 3baff94f390373e39fb047b960b54a425f3043d4

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name 
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add x, y
+    x + y
+end  
+
+def halve x
+    if x.class != Integer
+        return nil
+    end
+
+    x / 2
+end
